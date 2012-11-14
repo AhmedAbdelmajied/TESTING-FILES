39a1f3b9ea6be984b96e82335fa57774ac9ba227
Ahmed Amr AbdulFattah <AhmedAmrOn@hotmail.com>
2012-11-14 02:48:05 +0200

    Update

--- Test.c
+++ Test.c
@@ -9,24 +9,6 @@
 #include <stdio.h>
 int main(int argc, const char * argv[])
 {
-    int T , i=0;
-     double count = 1;
-     short current[100];
-    scanf("%d",&T);
-    
-    for (i = 0 ; i < T ; i++) {
-        scanf("%hd",&current[i]);
-    }
-    for ( i = 0 ; i < T ; i++) {
-        short currentShort = current[i];
-        while (currentShort>1) {
-            count *= currentShort;
-            currentShort--;
-        }
-        
-         printf("%f\n",count);
-        count=1;
-    }
-     return 0 ;
+
 }
    
